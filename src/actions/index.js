548718fbb75def6398463614ab341654e4239edd
Henry Chan <henry2014ips@gmail.com>
2021-09-11 22:38:03 +0800

    Add new types in constants

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,18 @@
 import { v4 as uuidv4 } from "uuid";
-import * as types from "./Types";
+import * as types from "../constants/ActionTypes";
 
 export const addTodo = (text) => ({
   type: types.ADD_TODO,
   id: uuidv4(),
   text,
+});
+
+export const toggleTodo = (id) => ({
+  type: types.TOGGLE_TODO,
+  id,
+});
+
+export const setTodoFilter = (filter) => ({
+  type: types.SET_TODO_FILTER,
+  filter,
 });
