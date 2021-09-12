8e93a88b5f54416a35e91a420cc66ed1eaeaf85a
Henry Chan <henry2014ips@gmail.com>
2021-09-12 18:08:02 +0800

    Updates

--- src/constants/ActionTypes.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TODO = "ADD_TODO";
-export const DELETE_TODO = "DELETE_TODO";
-export const ADD_MULTI_TODO = "ADD_MULTI_TODO";
-export const TOGGLE_TODO = "TOGGLE_TODO";
-export const SET_TODO_FILTER = "SET_TODO_FILTER";
